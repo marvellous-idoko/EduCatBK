94b8b64eadfefa318707dac99b0a8a1c36d68d89
CaptJackSparrow <idokomarvelous@gmail.com>
2022-09-08 15:20:17 +0100

    cors Error thursday sept 22

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ var express = require("express");
 var app = express();
 
 var cors = require("cors");
-// app.use(cors({origin: 'https://reportkad.netlify.app'}));
+app.use(cors({origin: 'https://reportkad.netlify.app'}));
 // app.use(cors({origin: 'http://localhost:4200'}));
 
 var mongoose = require("mongoose");
 const master = require('./master')
 const school = require('./school')
 
-app.use(cors());
-app.options('*', cors())
+// app.use(cors());
+// app.options('*', cors())
 
 
 app.use(express.json());
