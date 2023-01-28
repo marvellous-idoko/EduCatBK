974ff16225360ea25f1c57b687c13d1b22481bc4
CaptJackSparrow <idokomarvelous@gmail.com>
2023-01-28 04:45:38 +0100

    28 1 223

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ var mongoose = require("mongoose");
 const master = require('./master')
 const school = require('./school')
 
-// app.use(cors());
-// app.options('*', cors())
-
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 let URL = 'mongodb+srv://CaptJackSparrow:GcLNtd0BR6xiW11b@educatcluster0.xr1hmp5.mongodb.net/?retryWrites=true&w=majority'
